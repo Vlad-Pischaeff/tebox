636ceb261ca987089f1cc176f4b85e41cb7bcd29
Vlad-Pischaeff <pischaeff@gmail.com>
2023-02-22 09:47:49 +0500

    fix: update component

--- packages/webpanel/src/components/ChatBubble.tsx
+++ packages/webpanel/src/components/ChatBubble.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
-import { useChatContext } from 'store';
 import { iMessage } from  'types/types.context';
 import s from 'styles/MainChat.module.sass';
 
@@ -9,23 +8,17 @@ interface iProps extends React.HTMLAttributes<HTMLDivElement>{
 }
 
 export const ChatBubble = ({ msg }: iProps) => {
-    const { userId } = useChatContext();
 
     return (
-        <div
-            className={`
-                ${s.MsgContainer}
-                ${msg.from === userId ? s.right : s.left}
-            `}
-            key={Date.now()}
-            role="listitem"
-        >
+        <div className={s.MsgContainer}>
             <div className={s.date}>
-                { formatDistanceToNow(msg.date, {addSuffix: true, includeSeconds: true}) }
+                {
+                    formatDistanceToNow(
+                        msg.date,
+                        { addSuffix: true, includeSeconds: true }
+                    )
+                }
             </div>
-
-            {/* <div className={s.msg}>from {msg.from}</div>
-            <div className={s.msg}>to {msg.to}</div> */}
 
             <div className={s.msg}>{msg.message}</div>
         </div>
--- packages/webpanel/src/components/MainChat.tsx
+++ packages/webpanel/src/components/MainChat.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useChatContext } from 'store';
-import { iChat, iMessage } from  'types/types.context';
+import { iMessage } from  'types/types.context';
 import { ChatBubble } from './ChatBubble';
 import s from 'styles/MainChat.module.sass';
 
 export const MainChat = () => {
-    const { userId, managerId, chat } = useChatContext();
+    const { chat, userId } = useChatContext();
 
     const GROUPED = chat.reduce((r: iMessage[][], o: iMessage, i: number, a: iMessage[]) => {
         if (a[i].from === (a[i - 1] && a[i - 1].from)) {
@@ -21,11 +21,19 @@ export const MainChat = () => {
     return (
         <div className={s.MainContainer} role="listbox">
             {
-                GROUPED.map((group) => (
-                    <div key={Date.now()} className={s.MsgGroup} role="listbox">
+                GROUPED.map((group, idx) => (
+                    <div key={idx} className={s.MsgGroup} role="listbox">
                         {
-                            group.map((msg) => (
-                                <ChatBubble msg={msg} />
+                            group.map((msg, i) => (
+                                <div
+                                    className={`
+                                        ${msg.from === userId ? s.right : s.left}
+                                    `}
+                                    key={i}
+                                    role="listitem"
+                                >
+                                    <ChatBubble msg={msg} />
+                                </div>
                             ))
                         }
                     </div>
