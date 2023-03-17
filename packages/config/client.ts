94234b710220300c42911c1578dfc33f795b211e
Vlad-Pischaeff <pischaeff@gmail.com>
2023-03-17 12:28:15 +0500

    fix: update config

--- packages/config/client.ts
+++ packages/config/client.ts
@@ -1,18 +1,16 @@
 const config = {
-    SERVER_PORT:        process.env.REACT_APP_SERVER_PORT,
-    SERVER_ADDR:        process.env.REACT_APP_SERVER_ADDR,
-    SERVER_PROTO:       process.env.REACT_APP_SERVER_PROTO,
-    WEBSOCKET_PROTO:    process.env.REACT_APP_SERVER_PROTO === 'https'
-                            ? 'wss:'
-                            : 'ws:',
-    WEBSOCKET_ADDR:     '',
-    HOST:               'http://localhost:3000',
-    LIFETIME:           '10m',
+    SERVER_PORT:            process.env.REACT_APP_SERVER_PORT,
+    SERVER_ADDR:            process.env.REACT_APP_SERVER_ADDR,
+    SERVER_PROTO:           process.env.REACT_APP_SERVER_PROTO,
+    WEBSOCKET_PROTO:        '',
+    WEBSOCKET_ADDR:         '',
+    HOST:                   'http://localhost:3000',
+    LIFETIME:               '10m',
 
-    SMTP_HOST:          process.env.SMTP_HOST,
-    SMTP_PORT:          process.env.SMTP_PORT,
-    SMTP_USER:          process.env.SMTP_USER,
-    SMTP_PASS:          process.env.SMTP_PASS,
+    SMTP_HOST:              process.env.SMTP_HOST,
+    SMTP_PORT:              process.env.SMTP_PORT,
+    SMTP_USER:              process.env.SMTP_USER,
+    SMTP_PASS:              process.env.SMTP_PASS,
 
     REQUEST_AUTHKEY:        'Authorization',
     ACCESS_JWT_SECRET:      process.env.ACCESS_JWT_SECRET,
@@ -20,14 +18,16 @@ const config = {
     REFRESH_JWT_SECRET:     process.env.REFRESH_JWT_SECRET,
     REFRESH_JWT_LIFETIME:   process.env.REFRESH_JWT_LIFETIME,
 
-    JWT_HEADER: {
-        alg: 'RS256',
-        typ: 'JWT'
-    },
+    MDB_SERVER:             process.env.MDB_SERVER,
+    MDB_DATABASE:           process.env.MDB_DATABASE,
 
-    MDB_SERVER:         process.env.MDB_SERVER,
-    MDB_DATABASE:       process.env.MDB_DATABASE,
+    JWT_HEADER:             {
+                                alg: 'RS256',
+                                typ: 'JWT'
+                            },
 };
+
+config.WEBSOCKET_PROTO = config.SERVER_PROTO === 'https' ? 'wss:' : 'ws:';
 
 const { WEBSOCKET_PROTO, SERVER_ADDR, SERVER_PORT } = config;
 
