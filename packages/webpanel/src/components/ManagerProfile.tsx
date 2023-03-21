69fed7f5f94b99f2be966f610e4c52052b9ea33d
Vlad-Pischaeff <pischaeff@gmail.com>
2023-03-21 15:52:04 +0500

    refactor: update styles

--- packages/webpanel/src/components/ManagerProfile.tsx
+++ packages/webpanel/src/components/ManagerProfile.tsx
@@ -7,50 +7,41 @@ export const ManagerProfile = () => {
     // eslint-disable-next-line
     const { mngProfile, isMail, setIsMail, isSWReady } = useChatContext();
 
-    const Toggler = (event: React.MouseEvent) => {
+    const handleToggle = (event: React.MouseEvent) => {
         const check = event.currentTarget.id === 'mail';
         setIsMail(check);
     }
 
-    if (mngProfile) {
-        return (
-            <>
-                <figure className={s.avatar}>
-                    <img src={mngProfile.image} alt="avatar" />
-                </figure>
-                <div className={s.description}>
-                    {mngProfile.alias ? mngProfile.alias : mngProfile.name}
-                    {/* for testing purposes */}
-                    <div className={`${s.descriptionSw} ${isSWReady ? s.lb : s.lc}`}></div>
-                    {/* for testing purposes */}
-                </div>
-                <div className={s.menu}>
-                    { isMail
-                        ?   <div id="chat" className={s.menuItem} onClick={Toggler}>
-                                <img src={UserIcons.SVG.chat} alt="chat" />
-                            </div>
-                        :   <div id="mail" className={s.menuItem} onClick={Toggler}>
-                                <img src={UserIcons.SVG.mail} alt="mail" />
-                            </div>
-                    }
-                </div>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <div className={s.loader}>Loading manager profile...</div>
-                <div className={s.menu}>
-                    { isMail
-                        ?   <div id="chat" className={s.menuItem} onClick={Toggler}>
-                                <img src={UserIcons.SVG.chat} alt="chat" />
-                            </div>
-                        :   <div id="mail" className={s.menuItem} onClick={Toggler}>
-                                <img src={UserIcons.SVG.mail} alt="mail" />
-                            </div>
-                    }
-                </div>
-            </>
-        )
-    }
+    return (
+        <>
+            <figure className={s.avatar}>
+                {  !!mngProfile
+                    ?   <img src={mngProfile.image} alt="avatar" />
+                    :   <div>Avatar</div>
+                }
+            </figure>
+
+
+            <div className={s.description}>
+                { !!mngProfile
+                    ?   (mngProfile.alias || mngProfile.name)
+                    :   'Loading profile...'
+                }
+                {/* for testing purpose */}
+                <div className={`${s.descriptionSw} ${isSWReady ? s.lb : s.lc}`}></div>
+                {/* for testing purpose */}
+            </div>
+
+            <div className={s.menu}>
+                { isMail
+                    ?   <div id="chat" className={s.menuItem} onClick={handleToggle}>
+                            <img src={UserIcons.SVG.chat} alt="chat" />
+                        </div>
+                    :   <div id="mail" className={s.menuItem} onClick={handleToggle}>
+                            <img src={UserIcons.SVG.mail} alt="mail" />
+                        </div>
+                }
+            </div>
+        </>
+    );
 };
