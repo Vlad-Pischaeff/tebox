06f01732758f182ff290bc6c9d73cd967325f036
Vlad-Pischaeff <pischaeff@gmail.com>
2023-04-10 13:54:08 +0500

    fix: update types

--- packages/webadmin/src/pages/Dashboard/MainChat/Services/ServicesVariables.tsx
+++ packages/webadmin/src/pages/Dashboard/MainChat/Services/ServicesVariables.tsx
@@ -4,11 +4,11 @@ import { Mail } from 'pages/Dashboard/MainChat/Services/Mail';
 import { Notes } from 'pages/Dashboard/MainChat/Services/Notes';
 import { Answers } from 'pages/Dashboard/MainChat/Services/Answers';
 
-export const MENU = [ "Todos", "Notes", "Mail", "Answers" ] as const;
+export const MENU = [ "Todos", "Notes", "Mail", "Answers" ];
 
 export type tServiceMenu = typeof MENU[number];
 
-type tSymbol = {
+type tMenu = {
     [key in tServiceMenu]: () => JSX.Element
 }
 
@@ -19,7 +19,7 @@ export const BODY = [
     { name: MENU[3], render: () => <Answers /> },
 ];
 
-export const BODY_OBJ = BODY.reduce((next: tSymbol, val) => {
+export const BODY_OBJ = BODY.reduce((next: tMenu, val) => {
     next[val.name] = val.render;
     return next;
-}, {} as tSymbol);
+}, {} as tMenu);
