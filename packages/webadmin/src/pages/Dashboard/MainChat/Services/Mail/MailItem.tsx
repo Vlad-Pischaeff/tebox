9db3c2a5b8c0b6e7744048f0b37f45dd921a9ad9
Vlad-Pischaeff <pischaeff@gmail.com>
2023-04-12 08:38:04 +0500

    fix: add delete mail

--- packages/webadmin/src/pages/Dashboard/MainChat/Services/Mail/MailItem.tsx
+++ packages/webadmin/src/pages/Dashboard/MainChat/Services/Mail/MailItem.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import parse from 'html-react-parser';
 import { removeContentEditableAttr } from '@tebox/utils/lib';
+import { useDeleteMailMutation } from 'store/api/mailsApi';
 import { iMails } from 'store/api/apiTypes';
+import * as ICON from 'assets/icons';
 import s from '../Services.module.sass';
 
 interface iProps {
@@ -10,16 +12,28 @@ interface iProps {
 }
 
 export const MailItem = ({ mail }: iProps) => {
+    const [ deleteMail ] = useDeleteMailMutation();
+
+    const removeItem = () => {
+        deleteMail(mail.id);
+    }
+
     return (
         <div className={s.mailContainer} role="listitem">
-            <p>
-                <span className={s.gray}> from: </span>
-                <Link className={s.link} to={`mailto:${mail.from}`}>
-                    {mail.from}
-                </Link>
-                <span className={s.gray}> at: </span>
-                {new Date(mail.date).toLocaleDateString()}
-            </p>
+            <div className={s.mailHeader}>
+                <p>
+                    <span className={s.gray}> from: </span>
+                    <Link className={s.link} to={`mailto:${mail.from}`}>
+                        {mail.from}
+                    </Link>
+                    <span className={s.gray}> at: </span>
+                    {new Date(mail.date).toLocaleDateString()}
+                </p>
+                <div className={s.propertyIcon} onClick={removeItem}>
+                    <ICON.TrashIcon />
+                </div>
+            </div>
+
             <div className={s.mailBody}>
                 { parse(removeContentEditableAttr(mail.message)) }
             </div>
