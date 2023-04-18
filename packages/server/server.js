7b2669b93bea3016e603d4d54c5cd2c38b4220a1
Vlad-Pischaeff <pischaeff@gmail.com>
2023-04-18 09:24:27 +0500

    fix: update

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,24 +1,22 @@
 'use strict';
 
-let privateKey, certificate, credentials;
+const config = require('@tebox/config/server');
+let privateKey, certificate, credentials, server;
 
 const fs = require('fs');
 const express = require('express');
 const app = express();
 
-const isProduction = process.env.NODE_ENV === 'production'
-                        ? true
-                        : false;
+const isHTTPs = config.SERVER_PROTO === "http" ? false : true;
 
-if (isProduction) {
+if (isHTTPs) {
     privateKey = fs.readFileSync('./keys/privkey.pem', { encoding: 'utf8', flag: 'r' });
     certificate = fs.readFileSync('./keys/cert.pem', { encoding: 'utf8', flag: 'r' });
     credentials = { key: privateKey, cert: certificate };
+    server = require('https').createServer(credentials, app);
+} else {
+    server = require('http').createServer(app);
 }
-
-const server = isProduction
-                    ? require('https').createServer(credentials, app)
-                    : require('http').createServer(app);
 
 require('#s/startup/db')();
 require('#s/startup/cors')(app);
